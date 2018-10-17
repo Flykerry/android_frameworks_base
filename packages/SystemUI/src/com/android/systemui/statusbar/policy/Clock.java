219d16960e582dd3b7a95fdbed05a061709fc27d
Haibo Gu <ghbhaha@users.noreply.github.com>
2018-10-17 10:39:51 +0800

    Call location

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -380,9 +380,7 @@ public class Clock extends TextView implements DemoMode, Tunable, CommandQueue.C
 
         CharSequence dateString = null;
 
-        String result = "";
-        String timeResult = sdf.format(mCalendar.getTime());
-        String dateResult = "";
+        String result = is24 ? sdf.format(mCalendar.getTime()) : DateFormat.format(format, mCalendar.getTime()).toString();
 
         if (mClockDateDisplay != CLOCK_DATE_DISPLAY_GONE) {
             Date now = new Date();
